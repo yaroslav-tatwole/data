6f92565b64845a19dca4fb740249e95c1fb988ed
yaroslav_ka3131 <yaroslavka.krp7@gmail.com>
2025-01-23 09:59:28 +0700

    Add files via upload

--- 22.01.py
+++ 22.01.py
@@ -1,15 +1,5 @@
 import pandas as pd
 
+df_excel = pd.read_excel('data1.xlsx')
 
-
-# Чтение Excel-файла
-df_excel = pd.read_excel('data.xlsx')
-
-# Посмотреть первые 10 строк
-print(df_excel.head(10))
-
-print(df_excel.sample(10))
-
-# Посмотреть информацию о таблице
-print(df_excel.info())
-
+print(df_excel.iloc[1])
